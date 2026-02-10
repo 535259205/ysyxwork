6688068a680f8a3b0a092b4903246e008d256eff
tracer-ysyx <tracer@ysyx.org>
2026-02-10 16:10:32 +0800

    > sim RTL ysyx_22040000 刘明举 Linux DESKTOP-J45IO8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:10:32 up  2:20,  0 user,  load average: 1.06, 1.23, 0.95

--- npc/csrc/sim.cpp
+++ npc/csrc/sim.cpp
@@ -91,7 +91,7 @@ void SimInit(int argc, char **argv)
     #endif
 
     //设备复位
-    for(int i=0;i<=20;i++){
+    for(int i=0;i<=30-1;i++){
         dut->clock=!dut->clock;
         dut->reset = 1;
         dut->eval();
