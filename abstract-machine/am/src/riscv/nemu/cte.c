b6cd8568ce05fb5c3cf9422b925bad9206924ba0
tracer-ysyx <tracer@ysyx.org>
2026-02-28 17:26:12 +0800

    > compile NEMU ysyx_22040000 刘明举 Linux DESKTOP-J45IO8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:26:12 up  7:57,  0 user,  load average: 0.49, 0.43, 0.47

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -58,7 +58,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   // 设置参数寄存器 A0
   ctx->gpr[10] = (uintptr_t)arg;  // a0寄存器保存调用函数的第一个参数指针
   // ctx->gpr[2] = (uintptr_t)kstack.end;
-  ctx->gpr[2] =((uintptr_t)kstack.end - sizeof(Context) -4 );
+  ctx->gpr[2] =((uintptr_t)kstack.end - sizeof(Context));
   ctx->mcause = 0x08;
   ctx->mstatus = 0x00202122;  // MIE = 1
   // 要设置mepc为入口函数的地址 mret会进行如果mepc+4
