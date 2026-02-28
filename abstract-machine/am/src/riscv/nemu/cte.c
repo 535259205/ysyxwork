8d739d05bc1bcab38f6096ee63ea905a7ab4119f
tracer-ysyx <tracer@ysyx.org>
2026-02-28 16:16:41 +0800

    > compile NEMU ysyx_22040000 刘明举 Linux DESKTOP-J45IO8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  16:16:41 up  6:47,  0 user,  load average: 0.54, 0.98, 0.93

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,6 +6,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 //C实际就是栈指针 然后从yield触发ecall后开始传递
 Context* __am_irq_handle(Context *c) {
+  printf("mcause is %d\n", c->mcause);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
