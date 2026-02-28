68908cb315fbe109bd9945de3f4ae0ed5ad4c562
tracer-ysyx <tracer@ysyx.org>
2026-02-28 15:44:17 +0800

    > compile NEMU ysyx_22040000 刘明举 Linux DESKTOP-J45IO8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:44:17 up  6:15,  0 user,  load average: 0.55, 0.72, 0.73

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -157,7 +157,7 @@ static int decode_exec(Decode *s) {
     }); // 进入中断地址
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , CSR, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   //同步异常+4 异步异常（外部中断不+4）
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret , CSR, s->dnpc = (cpu.mepc));//返回被打断处的程序继续运行
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret , CSR, s->dnpc = (cpu.mepc+4));//返回被打断处的程序继续运行
   
   // CSRRW CSR读写指令
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , CSR, { 
