8ef75076e45d1a5859fa0f5b5e680bfef6993d39
tracer-ysyx <tracer@ysyx.org>
2026-03-26 15:45:27 +0800

    > compile NEMU ysyx_22040000 刘明举 Linux DESKTOP-J45IO8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:45:27 up  1:50,  0 user,  load average: 0.31, 0.44, 0.44

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-
+#include "amdev.h"
 extern char _heap_start;
 int main(const char *args);
 
@@ -10,12 +10,18 @@ static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); /
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
+
+#define NAMEINIT(key)  [ AM_KEY_##key ] = #key,
+static const char *names[] = {
+  AM_KEYS(NAMEINIT)
+};
+
 char getch(void)
 {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE || ev.keydown==0) return -1;
 
-  return ev.keycode;
+  return *names[ev.keycode];
 }
 
 void halt(int code) {
