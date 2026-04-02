09ebd33f44fb20f887f2b39d28d35ea15975712c
tracer-ysyx <tracer@ysyx.org>
2026-04-02 14:23:31 +0800

    > compile NEMU ysyx_26010010 liumingju Linux DESKTOP-J45IO8T 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:23:31 up 24 min,  0 user,  load average: 0.26, 0.29, 0.22

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,6 +9,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case 0x0B:
       case 0x08:
         // 检查a7寄存器的值，确定是yield请求
         if (c->GPR1 == -1)   // x17是a7寄存器
